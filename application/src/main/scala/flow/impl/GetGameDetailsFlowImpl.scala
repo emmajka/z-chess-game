539b6a6f457ef94dad4be860c34115a246966e3b
Em Majka <maciejmajka001@gmail.com>
2024-06-30 19:45:51 +0200

    minor refactor

--- application/src/main/scala/flow/impl/GetGameDetailsFlowImpl.scala
+++ application/src/main/scala/flow/impl/GetGameDetailsFlowImpl.scala
@@ -9,11 +9,8 @@ import zio.{Task, ZIO, ZLayer}
 case class GetGameDetailsFlowImpl(gameRepository: GameRepository) extends GetGameDetailsFlow {
   override def run(gameId: String): Task[GetGameDetailsResult] =
     for
-      _ <- ZIO.logInfo(s"retrieval of game details for game with ID $gameId")
-      gameDetails <- gameRepository.getGameDetails(gameId = gameId)
-      _ <- ZIO.fail(GameNotExists(gameId = gameId)).when(gameDetails.isEmpty)
+      gameDetails <- gameRepository.getGameDetails(gameId = gameId).filterOrFail(_.nonEmpty)(GameNotExists(gameId = gameId))
       pieces <- gameRepository.getGamePiecesDetails(gameId = gameId)
-      _ <- ZIO.logInfo(s"pieces: ${pieces.size}")
     yield GetGameDetailsResult(gameId = gameId, pieces = pieces)
 
 }
