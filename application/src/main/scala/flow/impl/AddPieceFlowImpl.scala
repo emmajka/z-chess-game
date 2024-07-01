cb9bc01b6bcdf06cabdb7a16248c02304ad3b777
Em Majka <maciejmajka001@gmail.com>
2024-07-01 19:49:36 +0200

    add new piece flow changes

--- application/src/main/scala/flow/impl/AddPieceFlowImpl.scala
+++ application/src/main/scala/flow/impl/AddPieceFlowImpl.scala
@@ -10,10 +10,10 @@ case class AddPieceFlowImpl(gameRepository: GameRepository) extends AddPieceFlow
   override def run(gameId: String, pieceType: PieceType, targetPosition: Position): Task[Int] =
     for
       gameDetails <- gameRepository.getGameDetails(gameId = gameId).filterOrFail(_.nonEmpty)(GameNotExists(gameId = gameId))
-      gamePieces <- gameRepository.getGamePiecesDetails(gameId = gameId)
-      pieceWithSamePosition = gamePieces.find(gp => gp.xCoordinate == targetPosition.x && gp.yCoordinate == targetPosition.y && gp.active)
+      allActivePieces <- gameRepository.getGamePiecesDetails(gameId = gameId).map(_.filter(_.active))
+      pieceWithSamePosition = allActivePieces.find(gp => gp.xCoordinate == targetPosition.x && gp.yCoordinate == targetPosition.y && gp.active)
       _ <- ZIO.foreachDiscard(pieceWithSamePosition)(p => ZIO.fail(PiecePlaceTaken(gameId = gameId, pieceId = p.pieceId, pieceCoordinates = targetPosition)))
-      newPieceId = gamePieces.maxByOption(_.pieceId).map(_.pieceId + 1).getOrElse(1)
+      newPieceId = allActivePieces.maxByOption(_.pieceId).map(_.pieceId + 1).getOrElse(1)
       _ <- gameRepository.addNewGamePiece(gameId = gameId, pieceId = newPieceId, pieceType = pieceType, coordinates = targetPosition)
     yield newPieceId
 
